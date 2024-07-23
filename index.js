a38c7b98091d5c47f687c0693194c83a81653a3f
Mike Ortiz <mike.ortiz@hginsights.com>
2024-07-22 17:02:57 -0700

    implement post route

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 import express from "express";
 import process from "process";
 import { getAllSongs } from "./methods.js";
+import { fullMusicList } from "./music_list.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.status(200).send({ allSongs: getAllSongs() });
 });
 
-// app.post("/song", (req, res) => {
-//   if (!req.body.name) {
-//     return res.status(400).json({ message: "Song name is required" });
-//   }
-//   if(!req.body.artist) {
-//     return res.status(400).json({ message: "Artist name is required" });
-//   }
-//   res.status(201).send(`Song - ${req.body.name} by ${req.body.artist} - created successfully`);
-// });
+app.post("/song", (req, res) => {
+  //refactor check for required fields to be more efficient
+  if (!req.body.title) {
+    return res.status(400).json({ message: "Song title is required" });
+  }
+  if (!req.body.artist) {
+    return res.status(400).json({ message: "Artist name is required" });
+  }
+  if (!req.body.enteredBy) {
+    return res
+      .status(400)
+      .json({ message: "Name of person making the entry is required" });
+  }
+
+  const newSong = {
+    id: getAllSongs().length + 1,
+    name: req.body.name,
+    artist: req.body.artist,
+    album: req.body.album,
+    genre: req.body.genre,
+    duration: req.body.duration,
+    enteredBy: req.body.enteredBy,
+  };
+
+  fullMusicList.push(newSong);
+
+  res.status(201).send({
+    message: `Song - ${req.body.title} by ${req.body.artist} - added successfully`,
+    allSongs: getAllSongs(),
+  });
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
