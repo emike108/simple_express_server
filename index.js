40a4cd30bc95d0d1735716a2269e11e88ea1cbcc
Mike Ortiz <mike.ortiz@hginsights.com>
2024-09-04 11:40:56 -0700

    revert some files

--- index.js
+++ index.js
@@ -1,41 +1,5 @@
-import express from "express";
-import process from "process";
-import { getAllSongs } from "./methods.js";
-import { fullMusicList } from "./music_list.js";
-import { requiredFields } from "./routeUtils.js";
-
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
-
-app.use(express.json());
-
-app.get("/", (_req, res) => {
-  res.status(200).send({ allSongs: getAllSongs() });
-});
-
-app.post("/song", (req, res) => {
-  for (const { field, message } of requiredFields) {
-    if (!req.body[field]) {
-      return res.status(400).send({ message });
-    }
-  }
-
-  const newSong = {
-    id: getAllSongs().length + 1,
-    name: req.body.name,
-    artist: req.body.artist,
-    album: req.body.album,
-    genre: req.body.genre,
-    duration: req.body.duration,
-    enteredBy: req.body.enteredBy,
-  };
-
-  fullMusicList.push(newSong);
-
-  res.status(201).send({
-    message: `Song - ${req.body.title} by ${req.body.artist} - added successfully`,
-    allSongs: getAllSongs(),
-  });
-});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
